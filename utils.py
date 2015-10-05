ab8a0ff45e37df22c9d79fa41e3b7c5157b2b69b
ambodi <amir.rahnama@gmail.com>
2015-10-05 22:33:38 +0200

    normalizing tweets according to Alec Go's 2009 paper

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import re
+from nltk.corpus import stopwords
 
 
 class Util:
@@ -10,6 +11,14 @@ class Util:
         :return: normalized text according to: Alec Go (2009)'s Twitter Sentiment Classification using Distant Supervision
         """
         # http://stackoverflow.com/questions/2304632/regex-for-twitter-username
-        result = re.sub('(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)', 'USER', tweet)
+        tweet = unicode(tweet)
+        result = tweet.decode('unicode_escape').encode('ascii', 'ignore')
+        result = re.sub('(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)', 'USER', result)
+        result = result.replace('\\', '')
         result = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', 'URL', result)
-        return result
+        result = re.sub('[^a-zA-Z]', ' ', result)
+        result = result.lower().split()
+        stops = set(stopwords.words("english"))
+        tokens = [t for t in result if not t in stops and t.isalnum()]
+
+        return " ".join(tokens)
