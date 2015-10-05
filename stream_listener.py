c9c15ad4345d809699b818472b31169ef2941895
ambodi <amir.rahnama@gmail.com>
2015-10-05 22:38:54 +0200

    stream listener reads instances, passes them to the normalise

--- stream_listener.py
+++ stream_listener.py
@@ -1,12 +1,18 @@
 import tweepy
-import utils
+from pipe import pipe
+from utils import Util
 
 
 class TwitterStreamListener(tweepy.StreamListener):
 
-    def on_status(self, status):
-        print(status.text)
-        utils.Util.normalize(status.text)
+    def __init__(self):
+        self.pipe = pipe()
+
+    def on_data(self, data):
+        text_i = data.find('"text"')
+        source_i = data.find("source")
+        text = data[text_i + 8:source_i]
+        self.pipe.add(Util.normalize(text))
 
     def on_error(self, status_code):
         if status_code == 420:
